1db0f4c1e9f579254b944c18f1af5d5a31c21501
NICO GARCIA <07nico.garcia@gmail.com>
2025-07-18 12:39:27 +0200

    fix:creation page ux ui improvement

--- client/src/pages/recipeCreation/recipeCreation.tsx
+++ client/src/pages/recipeCreation/recipeCreation.tsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
+import { Bounce, ToastContainer, toast } from "react-toastify";
 
 import ImgUpload from "../../components/ImgUpload/ImgUpload";
 
@@ -102,7 +103,7 @@ export default function recipeCreation() {
     }
   };
 
-  const onSubmit: SubmitHandler<RecipeForm> = (data) => {
+  const onSubmit: SubmitHandler<RecipeForm> = async (data) => {
     const formData = new FormData();
     formData.append("title", data.title);
     formData.append("persons", data.persons);
@@ -115,18 +116,37 @@ export default function recipeCreation() {
     formData.append("image", data.image[0]);
 
     try {
-      axios
-        .post(`${import.meta.env.VITE_API_URL}/api/recipe`, formData, {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then(() => {
-          navigate("/");
-        });
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/recipe`, formData, {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setTimeout(() => navigate("/"), 3500);
+      toast.success("Recette créée avec succès!", {
+        position: "top-right",
+        autoClose: 3400,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     } catch (err) {
       console.error(err);
+      toast.error("Erreur lors de la création de la recette", {
+        position: "top-right",
+        autoClose: 3400,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
   };
 
@@ -428,6 +448,7 @@ export default function recipeCreation() {
           >
             Envoyer
           </motion.button>
+          <ToastContainer />
         </div>
       </form>
     </>
