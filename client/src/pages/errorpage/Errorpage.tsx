dc44aedd6db367611fc6f3690117b2b2b925d331
GARY GRAS <gary.gras07@gmail.com>
2025-07-22 11:58:13 +0200

    feat: error page

---
+++ client/src/pages/errorpage/Errorpage.tsx
@@ -0,0 +1,12 @@
+import "./errorpage.css";
+
+function ErrorPage() {
+  return (
+    <div className="errorpage">
+      <div className="error-image" />
+      <div className="error-content"/>
+    </div>
+  );
+}
+
+export default ErrorPage;
