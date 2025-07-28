86cefaafbae9fe2ff82c9d1f6535ded84fece267
NICO GARCIA <07nico.garcia@gmail.com>
2025-07-28 10:26:25 +0200

    feat: ingredient calories input admin side

--- client/src/components/DashboardAdmin/IngredientAdmin.tsx
+++ client/src/components/DashboardAdmin/IngredientAdmin.tsx
@@ -3,6 +3,9 @@ import type { AdminIngredientsType } from "../../lib/definition";
 
 export default function IngredientAdmin() {
   const [ingredients, setIngredients] = useState<AdminIngredientsType[]>([]);
+  const [caloriesValues, setCaloriesValues] = useState<Record<number, string>>(
+    {},
+  );
 
   const fetchIngredients = useCallback(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/admin`)
@@ -14,6 +17,32 @@ export default function IngredientAdmin() {
   useEffect(() => {
     fetchIngredients();
   }, [fetchIngredients]);
+
+  const handleClick = (ing: AdminIngredientsType) => {
+    const calories = caloriesValues[ing.id];
+    try {
+      if (calories) {
+        fetch(
+          `${import.meta.env.VITE_API_URL}/api/admin/ingredients/${ing.id}`,
+          {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              nom: ing.nom,
+              calories: calories,
+              is_validated: ing.is_validated,
+            }),
+          },
+        ).then((response) => {
+          if (response.ok) {
+            fetchIngredients();
+          }
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="admin-table">
@@ -28,7 +57,32 @@ export default function IngredientAdmin() {
         <ul className="table-header-data" key={ing.id}>
           <li>{ing.id}</li>
           <li>{ing.nom}</li>
-          <li>{ing.calories} </li>
+          <li>
+            {ing.calories !== null ? (
+              ing.calories
+            ) : (
+              <div className="calories-placeholder">
+                <input
+                  className="calories-input"
+                  type="number"
+                  placeholder=" calories"
+                  onChange={(e) =>
+                    setCaloriesValues({
+                      ...caloriesValues,
+                      [ing.id]: e.target.value,
+                    })
+                  }
+                />
+                <button
+                  type="button"
+                  className="button-admin"
+                  onClick={() => handleClick(ing)}
+                >
+                  Valider
+                </button>
+              </div>
+            )}
+          </li>
           <li>{ing.is_validated ? "Validé" : "En attente"}</li>
           <li>
             <button
