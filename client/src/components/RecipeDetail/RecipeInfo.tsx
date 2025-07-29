405f2be62050e7157868151e9aa6cac60e67a4a8
NabilY-5 <nabilyahya59280@gmail.com>
2025-07-29 16:04:55 +0200

    fix: check resolved in RecipeInfo

--- client/src/components/RecipeDetail/RecipeInfo.tsx
+++ client/src/components/RecipeDetail/RecipeInfo.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import type { recipeType } from "../../lib/definition";
 import Favorite from "../../components/RecipeDetail/Favorite";
+import type { recipeType } from "../../lib/definition";
 
 type transitionType = {
   [key: number]: string;
